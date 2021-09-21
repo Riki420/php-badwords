b277b2d86f162b59dd0d4f388571a51c9e41532a
Riki420 <girottiriccardo1998@gmail.com>
2021-09-21 15:41:38 +0200

    create var paragraph + echo in body

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+
+<?php 
+
+    $paragraph= "First off, fuck your bitch and the click you claim
+                Westside when we ride come equipped with game
+                You claim to be a player but I fucked your wife
+                We bust on Bad Boy niggaz fucked for life
+                Plus Puffy tryin' ta see me weak hearts I rip
+                Biggie Smalls and Junior M.A.F.I.A. Some mark-ass bitches
+                We keep on comin' while we runnin' for yo' jewels
+                Steady gunnin, keep on bustin at them fools, you know the rules
+                Lil' Ceaser, go ask ya homie how I leave ya
+                Cut your young ass up, leave you in pieces, now be deceased
+                Lil' Kim, don't fuck around with real G's
+                Quick to snatch yo' ugly ass off the streets, so fuck peace
+                I let them niggaz know it's on for life
+                So let the Westside ride tonight
+                Bad Boy murdered on wax and killed
+                Fuck wit' me and get yo' caps peeled, you know, see"
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +30,6 @@
     <title>Bad Words</title>
 </head>
 <body>
-    
+    <?php echo $paragraph ?>
 </body>
 </html>
